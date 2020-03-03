70098792e7d83b76d431e866c97da0dd932ead62
Geoffrey Lovelace <geoffrey4444@gmail.com>
2020-03-03 10:38:58 -0800

    Fixup test writes and deletes test h5 file

--- tests/Unit/IO/Importers/Test_SpecFunctionOfTimeReader.cpp
+++ tests/Unit/IO/Importers/Test_SpecFunctionOfTimeReader.cpp
@@ -18,6 +18,9 @@
 #include "Domain/FunctionsOfTime/FunctionOfTime.hpp"
 #include "Domain/FunctionsOfTime/PiecewisePolynomial.hpp"
 #include "Domain/FunctionsOfTime/Tags.hpp"
+#include "IO/H5/AccessType.hpp"
+#include "IO/H5/Dat.hpp"
+#include "IO/H5/File.hpp"
 #include "IO/Importers/SpecFunctionOfTimeReader.hpp"
 #include "IO/Importers/Tags.hpp"
 #include "Informer/InfoFromBuild.hpp"
@@ -83,15 +86,52 @@ SPECTRE_TEST_CASE("Unit.Evolution.SpecFunctionOfTimeReader",
 
   const size_t self_id{1};
 
+  // Create a temporary file with test data to read in
+  // First, check if the file exists, and delete it if so
+  const std::string test_filename{"TestSpecFuncOfTimeData.h5"};
+  constexpr uint32_t version_number = 4;
+  if (file_system::check_if_file_exists(test_filename)) {
+    file_system::rm(test_filename, true);
+  }
+
+  h5::H5File<h5::AccessType::ReadWrite> test_file(test_filename);
+
+  const std::array<double, 2> expected_times{{0.0, 0.00599999974179903}};
+  const std::array<std::string, 2> expected_names{
+      {"ExpansionFactor", "RotationAngle"}};
+
+  const std::vector<std::vector<double>> test_expansion{
+      {5.9999997417990300e-03, 0.0, 1.0, 3.0, 1.0, 1.0, -1.4268296756999999e-06,
+       0.0, 0.0},
+      {5.9999997417990300e-03, 5.9999997417990300e-03, 1.0, 3.0, 1.0,
+       9.9999999143902230e-01, -1.4268296756999999e-06, 0.0,
+       -3.5943676411727592e-05}};
+  const std::vector<std::string> expansion_legend{
+      "Time", "TLastUpdate", "Nc",  "DerivOrder", "Version",
+      "a",    "da",          "d2a", "d3a"};
+  auto& expansion_file = test_file.insert<h5::Dat>(
+      "/" + expected_names[0], expansion_legend, version_number);
+  expansion_file.append(test_expansion);
+
+  const std::vector<std::vector<double>> test_rotation{
+      {5.9999997417990300e-03, 0.0, 1.0, 3.0, 1.0, 0.0, 1.3472907726000001e-02,
+       0.0, 0.0},
+      {5.9999997417990300e-03, 5.9999997417990300e-03, 1.0, 3.0, 1.0,
+       8.0837442877282155e-05, 1.3472907726000001e-02, 0.0,
+       1.4799266358888008e-04}};
+  const std::vector<std::string> rotation_legend{
+      "Time", "TLastUpdate", "Nc",    "DerivOrder", "Version",
+      "Phi",  "dPhi",        "d2Phi", "d3Phi"};
+  auto& rotation_file = test_file.insert<h5::Dat>(
+      "/" + expected_names[1], rotation_legend, version_number);
+  rotation_file.append(test_rotation);
+
   MockRuntimeSystem runner{{}};
   ActionTesting::emplace_component_and_initialize<component<Metavariables>>(
       &runner, self_id,
-      {std::string{
-           unit_test_path() +
-           "/Evolution/Initialization/FunctionOfTimeFromFile_TestData.h5"},
-       std::map<std::string, std::string>{
-           {"ExpansionFactor", "ExpansionFactor"},
-           {"RotationAngle", "RotationAngle"}}});
+      {std::string{test_filename}, std::map<std::string, std::string>{
+                                       {"ExpansionFactor", "ExpansionFactor"},
+                                       {"RotationAngle", "RotationAngle"}}});
   runner.set_phase(Metavariables::Phase::Testing);
   runner.next_action<component<Metavariables>>(self_id);
 
@@ -102,19 +142,16 @@ SPECTRE_TEST_CASE("Unit.Evolution.SpecFunctionOfTimeReader",
 
   // Check that the FunctionOfTime and its derivatives have the expected
   // values
-  const std::array<double, 2> expected_times{{0.0, 0.00599999974179903}};
-  const std::array<std::string, 2> expected_names{
-      {"RotationAngle", "ExpansionFactor"}};
   const std::array<std::array<double, 3>, 2> expected_expansion{
-      {{{1.000000000000000, -0.000001426829676, 0.0}},
-       {{0.999999991439022, -0.000001426829676, 0.0}}}};
+      {{{test_expansion[0][5], test_expansion[0][6], test_expansion[0][7]}},
+       {{test_expansion[1][5], test_expansion[1][6], test_expansion[1][7]}}}};
   const std::array<std::array<double, 3>, 2> expected_rotation{
-      {{{0.0, 0.013472907726000, 0.0}},
-       {{0.000080837442877, 0.013472907726000, 0.0}}}};
+      {{{test_rotation[0][5], test_rotation[0][6], test_rotation[0][7]}},
+       {{test_rotation[1][5], test_rotation[1][6], test_rotation[1][7]}}}};
   std::unordered_map<std::string, std::array<std::array<double, 3>, 2>>
       expected_functions;
-  expected_functions[expected_names[0]] = expected_rotation;
-  expected_functions[expected_names[1]] = expected_expansion;
+  expected_functions[expected_names[0]] = expected_expansion;
+  expected_functions[expected_names[1]] = expected_rotation;
 
   for (const auto& function_of_time : functions_of_time) {
     const auto& f = function_of_time.second;
@@ -132,4 +169,7 @@ SPECTRE_TEST_CASE("Unit.Evolution.SpecFunctionOfTimeReader",
       }
     }
   }
+
+  // Delete the temporary file created for this test
+  file_system::rm(test_filename, true);
 }
