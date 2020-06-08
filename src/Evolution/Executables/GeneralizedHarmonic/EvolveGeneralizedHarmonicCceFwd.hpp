dd288c865231bd326dbf0aa713ed35e92df9cf8f
Jordan Moxon <jem497@cornell.edu>
2020-06-07 22:51:24 -0700

    Add combined GH-CCE executable

---
+++ src/Evolution/Executables/GeneralizedHarmonic/EvolveGeneralizedHarmonicCceFwd.hpp
@@ -0,0 +1,29 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+
+/// \cond
+namespace GeneralizedHarmonic {
+template <size_t Dim>
+struct System;
+namespace Solutions {
+template <typename GrSolution>
+struct WrappedGr;
+}  // namespace Solutions
+}  // namespace GeneralizedHarmonic
+namespace gr {
+namespace Solutions {
+struct KerrSchild;
+}  // namespace Solutions
+}  // namespace gr
+namespace evolution {
+template <typename System>
+struct NumericInitialData;
+}  // namespace evolution
+
+template <typename InitialData, typename BoundaryConditions>
+struct EvolutionMetavars;
+/// \endcond
