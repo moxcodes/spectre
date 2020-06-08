984dd6720e155b250bb760aa4037a0c93ed14ebf
Jordan Moxon <jem497@cornell.edu>
2020-06-07 22:50:19 -0700

    Move first surface data to `Evolve` phase

--- src/Evolution/Systems/Cce/Components/CharacteristicEvolution.hpp
+++ src/Evolution/Systems/Cce/Components/CharacteristicEvolution.hpp
@@ -25,6 +25,7 @@
 #include "Evolution/Systems/Cce/ScriPlusValues.hpp"
 #include "Evolution/Systems/Cce/SwshDerivatives.hpp"
 #include "IO/Observer/ObserverComponent.hpp"
+#include "Parallel/Actions/Goto.hpp"
 #include "Parallel/Actions/TerminatePhase.hpp"
 #include "Parallel/GlobalCache.hpp"
 #include "ParallelAlgorithms/Actions/MutateApply.hpp"
@@ -35,6 +36,8 @@
 #include "Utilities/TMPL.hpp"
 
 namespace Cce {
+
+struct CceEvolutionLabelTag {};
 
 /*!
  * \brief The component for handling the CCE evolution and waveform output.
@@ -96,11 +99,6 @@ struct CharacteristicEvolution {
       tmpl::list<Actions::InitializeCharacteristicEvolutionVariables,
                  Actions::InitializeCharacteristicEvolutionTime,
                  Actions::InitializeCharacteristicEvolutionScri,
-                 Actions::RequestBoundaryData<
-                     typename Metavariables::cce_boundary_component,
-                     CharacteristicEvolution<Metavariables>>,
-                 Actions::ReceiveWorldtubeData<Metavariables>,
-                 Actions::InitializeFirstHypersurface,
                  Initialization::Actions::RemoveOptionsAndTerminatePhase>;
 
   using initialization_tags =
@@ -154,6 +152,12 @@ struct CharacteristicEvolution {
                  ::Actions::AdvanceTime>;
 
   using extract_action_list = tmpl::list<
+      Actions::RequestBoundaryData<
+          typename Metavariables::cce_boundary_component,
+          CharacteristicEvolution<Metavariables>>,
+      Actions::ReceiveWorldtubeData<Metavariables>,
+      Actions::InitializeFirstHypersurface,
+      ::Actions::Label<CceEvolutionLabelTag>,
       Actions::RequestNextBoundaryData<
           typename Metavariables::cce_boundary_component,
           CharacteristicEvolution<Metavariables>>,
@@ -163,7 +167,8 @@ struct CharacteristicEvolution {
       Actions::FilterSwshVolumeQuantity<Tags::BondiH>,
       compute_scri_quantities_and_observe, record_time_stepper_data_and_step,
       Actions::ExitIfEndTimeReached,
-      Actions::ReceiveWorldtubeData<Metavariables>>;
+      Actions::ReceiveWorldtubeData<Metavariables>,
+      ::Actions::Goto<CceEvolutionLabelTag>>;
 
   using phase_dependent_action_list =
       tmpl::list<Parallel::PhaseActions<typename Metavariables::Phase,
